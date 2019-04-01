522ce896397e371f3415cfe5aec65feb3b9fc754
Hagen <teraf99@gmail.com>
2019-04-01 15:24:17 +0200

    First commit, Editing in Condition

--- TombOfPik8/src/model/effects/Condition.java
+++ TombOfPik8/src/model/effects/Condition.java
@@ -1,4 +1,34 @@
 package model.effects;
 
 public enum Condition {
+    POISON, SLEEP;
+
+    private Effect effect;
+
+    public Effect getEffect () {
+        return this.effect;
+    }
+
+    private Condition () {
+        switch (this.name()) {
+            case "POISON":
+                this.effect = new Effect("Poison", 4) {
+                    @Override
+                    public void applyEffect() {
+                        //TODO implement an effect.
+                        System.out.println("POISON!!!11!!");
+                    }
+                };
+                break;
+            case "SLEEP":
+                this.effect = new Effect("Sleep", 4) {
+                    @Override
+                    public void applyEffect() {
+                        //TODO implement an effect.
+                    }
+                };
+                break;
+
+        }
+    }
 }
