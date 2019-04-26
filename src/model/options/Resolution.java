7fe7e3d2fb25186c817177d6521cf0a0e46d9ee1
fachinformatiker <01020102..aA>
2019-04-26 13:44:52 +0200

    implemented better options

---
+++ src/model/options/Resolution.java
@@ -0,0 +1,65 @@
+package model.options;
+
+import static model.options.Resolution.WindowSize.MEDIUM;
+
+public class Resolution {
+
+    public static Object WindowSize;
+
+    enum WindowSize {
+        LOW(858, 480, false),
+        MEDIUM(1280, 720, false),
+        HIGH(1920, 1080, false),
+        MEDIUM_FULL(1280, 720, true),
+        HIGH_FULL(1920, 1080, true);
+
+        private Integer width;
+        private Integer height;
+        private boolean isFullscreen;
+
+        WindowSize(Integer width, Integer height, boolean isFullscreen) {
+            this.width = width;
+            this.height = height;
+            this.isFullscreen = isFullscreen;
+        }
+
+        public Integer getWidth() {
+            return width;
+        }
+
+        public Integer getHeight() {
+            return height;
+        }
+
+        public Boolean getIfFullscreen() {
+            return isFullscreen;
+        }
+    }
+
+
+    public static void getSelectedWindowSize(Resolution.WindowSize winSiz) {
+
+        switch(winSiz) {
+            case LOW:
+                System.out.println("Window size is low - Width: " + winSiz.getWidth() + " x Height: " + winSiz.getHeight() + " fullscreen? " + winSiz.getIfFullscreen());
+                break;
+            case MEDIUM:
+                System.out.println("Window size is medium - Width: " + winSiz.getWidth() + " x Height: " + winSiz.getHeight() + " fullscreen? " + winSiz.getIfFullscreen());
+                break;
+            case HIGH:
+                System.out.println("Window size is high - Width: " + winSiz.getWidth() + " x Height: " + winSiz.getHeight() + " fullscreen? " + winSiz.getIfFullscreen());
+                break;
+            case MEDIUM_FULL:
+                System.out.println("Window size is medium and fullscreen - Width: " + winSiz.getWidth() + " x Height: " + winSiz.getHeight() + " fullscreen? " + winSiz.getIfFullscreen());
+                break;
+            case HIGH_FULL:
+                System.out.println("Window size is high and fullscreen - Width: " + winSiz.getWidth() + " x Height: " + winSiz.getHeight() + " fullscreen? " + winSiz.getIfFullscreen());
+                break;
+            default:
+                winSiz = MEDIUM;
+                System.out.println("Window size is medium - Width: " + winSiz.getWidth() + " x Height: " + winSiz.getHeight() + " fullscreen? " + winSiz.getIfFullscreen());
+                break;
+        }
+    }
+
+}
---
+++ src/model/options/Volume.java
@@ -0,0 +1,15 @@
+package model.options;
+
+import javafx.scene.control.Slider;
+
+public class Volume {
+
+    private Slider GeneralVolumeSlider;
+    private Slider EffectsVolumeSlider;
+    private Slider MusicVolumeSlider;
+
+    public static void getSelectedVolumes(){
+
+    }
+
+}
