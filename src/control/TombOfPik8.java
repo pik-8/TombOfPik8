06594a535b000992b29bb661b818f624c8a0283f
Ceebly <ben@hastedts.de>
2019-05-01 10:29:30 +0200

    Change some stuff in TombOfPik8.java for testing

--- src/control/TombOfPik8.java
+++ src/control/TombOfPik8.java
@@ -6,13 +6,10 @@ import com.google.gson.GsonBuilder;
 import model.effects.DamageEffect;
 import model.fighting.Attack;
 import model.fighting.AttackPattern;
-import model.dungeon.DungeonFactory;
-import model.dungeon.Landscape;
-import model.dungeon.Position;
-import model.items.Trinket;
-import model.items.TrinketFactory;
+import model.items.Rarity;
+import model.items.Weapon;
+import model.items.WeaponFactory;
 import model.json.AdapterFactories;
-import view.DungeonPrinter;
 
 /**
  * The project for the Prog2 module, in which we are supposed to create an dungeon-crawler
@@ -26,26 +23,28 @@ public class TombOfPik8 {
 
 
     public static void main (String args[]) {
-    	int i = 0;
-    	TrinketFactory wf = new TrinketFactory();
-		System.out.println(i++);
-		Trinket wp = wf.generateRandomTrinket(50);
+    	WeaponFactory wf = new WeaponFactory();
+		Weapon wp = wf.generateRandomWeapon(50, Rarity.EPIC);
 
 
-//    	System.out.println("Name: " + wp.getName());
-//    	System.out.println("Description: " + wp.getDescription());
-//    	System.out.println("Magic Defense: " + wp.getSecStats().getMagicDefence());
+    	System.out.println("Name: " + wp.getName());
+    	System.out.println("Description: " + wp.getDescription());
+    	System.out.println("Attack: " + wp.getSecStats().getAttack());
+    	System.out.println("Magic Attack: " + wp.getSecStats().getMagicAttack());
 
-    	try {
-    		Attack attack = new Attack("Basic Attack", "Hit them.", 100, 100, 100, 1, null, 
-    				new AttackPattern(new float[][] {{0,1,0},
-    					{0,-1,0}
-    				}, new DamageEffect[][] {{null, new DamageEffect(), null},{null, null, null}} ));    		
-    		generateTemplate(attack);
-    	} catch (Exception e) {
-    		System.out.println(e.getMessage());
-    	}
+//    	try {
+//    		Attack attack = new Attack("Basic Attack", "Hit them.", 100, 100, 100, 1, null, 
+//    				new AttackPattern(new float[][] {{0,1,0},
+//    					{0,-1,0}
+//    				}, new DamageEffect[][] {{null, new DamageEffect(), null},{null, null, null}} ));    		
+//    		generateTemplate(attack);
+//    	} catch (Exception e) {
+//    		System.out.println(e.getMessage());
+//    	}
     	
+//    	DungeonPrinter printer = new DungeonPrinter();
+//    	Dungeon dungeon = new DungeonFactory().generateRandomDungeon();
+//    	printer.printDungeon(dungeon);
     	
     }
 
